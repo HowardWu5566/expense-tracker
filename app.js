1657a75231fdb26ea98b16126533da72f92b8a5c
HowardWu5566 <ggg1237890@gmail.com>
2022-09-23 09:00:17 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars');
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
+
+require('./config/mongoose')
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
