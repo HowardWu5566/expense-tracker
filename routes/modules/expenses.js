c0b879aba8939041f6c049107cb9fc6fe83d96e5
HowardWu5566 <ggg1237890@gmail.com>
2022-09-24 15:58:01 +0800

    feat: add edit page

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -3,15 +3,22 @@ const Expense = require('../../models/expense')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-  const categoryList = require('../../utils/categoryList')
-  const categoryArr = Object.keys(categoryList)
+  const categoryArr = Object.keys(require('../../utils/categoryList'))
   res.render('new', { categoryArr })
 })
 
 router.post('/', (req, res) => {
-
   Expense.create(req.body)
     .then(res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  const categoryArr = Object.keys(require('../../utils/categoryList'))
+  return Expense.findById(id)
+    .lean()
+    .then(expense => res.render('edit', { expense, categoryArr }))
     .catch(error => console.log(error))
 })
 
