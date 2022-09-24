ef08a8f0824cd56734bf38a98d2361480e38cdd4
HowardWu5566 <ggg1237890@gmail.com>
2022-09-24 20:16:34 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-
+const methodOverride = require('method-override')
 require('dotenv').config()
 require('./config/mongoose')
 const routes = require('./routes')
+
 const app = express()
 const PORT = process.env.PORT
 
@@ -11,6 +12,7 @@ app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use(routes)
 
--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const Expense = require('../../models/expense')
 const router = express.Router()
+const Expense = require('../../models/expense')
+
 
 router.get('/new', (req, res) => {
   const categoryArr = Object.keys(require('../../utils/categoryList'))
@@ -19,6 +20,20 @@ router.get('/:id/edit', (req, res) => {
   return Expense.findById(id)
     .lean()
     .then(expense => res.render('edit', { expense, categoryArr }))
+    .catch(error => console.log(error))
+})
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  return Expense.findById(id)
+    // .lean()
+    .then(expense => {
+      expense.name = req.body.name
+      expense.date = req.body.date
+      expense.category = req.body.category
+      expense.amount = req.body.amount
+      return expense.save()
+    })
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
