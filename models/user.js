3409bbded86e1f9cb2e54f56269dc20234eab0ae
HowardWu5566 <ggg1237890@gmail.com>
2022-09-25 02:08:39 +0800

    feat: add register function

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schma = mongoose.Schema
+const userSchema = new Schma({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const User = require('../../models/user')
 const router = express.Router()
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('此信箱已註冊')
+        return res.render('register', { name, email, password, confirmPassword })
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({ name, email, password: hash }))
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+    })
 })
 
 router.get('/login', (req, res) => {
