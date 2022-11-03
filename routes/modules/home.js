0da37571695ee58589dc9abffa5976636c61befc
HowardWu5566 <ggg1237890@gmail.com>
2022-11-03 14:49:59 +0800

    feat: add filter-by-date function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,29 +1,38 @@
 const express = require('express')
 const router = express.Router()
 const dayjs = require('dayjs')
+const isSameOrAfter = require('dayjs/plugin/isSameOrAfter')
+const isSameOrBefore = require('dayjs/plugin/isSameOrBefore')
 const Record = require('../../models/record')
 const categoryList = require('../../models/seeds/category.json')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
-  const categoryName = req.query.category
+  const { category, startDate, endDate } = req.query
   Record.find({ userId })
     .populate('categoryId')
     .lean()
     .sort({ date: 1 })
     .then(expenses => {
       // 依類別篩選
-      if (categoryName && categoryName !== 'All') {
-        expenses = expenses.filter(expense => expense.categoryId.name === categoryName)
+      if (category && category !== 'All') {
+        expenses = expenses.filter(expense => expense.categoryId.name === category)
       }
-      // 計算總費用及修改日期格式
+      // 依日期篩選
+      if (startDate || endDate) {
+        dayjs.extend(isSameOrAfter)
+        dayjs.extend(isSameOrBefore)
+        expenses = expenses.filter(expense => dayjs(expense.date).isSameOrAfter(startDate))
+        expenses = expenses.filter(expense => dayjs(expense.date).isSameOrBefore(endDate))
+      }
+      // 計算總費用及自訂日期格式
       let totalAmount = 0
       expenses.forEach((expense, index) => {
         if (index % 2 === 0) expense.background = true
         totalAmount += expense.amount
         expense.date = dayjs(expense.date).format('YYYY/MM/DD')
       })
-      res.render('home', { expenses, categoryList, categoryName, totalAmount })
+      res.render('home', { expenses, categoryList, category, startDate, endDate, totalAmount })
     })
     .catch(error => console.log(error))
 })
